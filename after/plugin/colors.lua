ede54e907724dab39711bd8618319f11e8b7c7e9
Ivan Bykov <renbrek@gmail.com>
2023-10-03 10:40:16 +0300

    feat: add harpoon, undotree and fugitive

--- after/plugin/colors.lua
+++ after/plugin/colors.lua
@@ -2,11 +2,11 @@ function ColorMyPencils(color)
 	color = color or "kanagawa-dragon"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
-    vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})
-    vim.api.nvim_set_hl(0, "SignColumn", {bg = "none"})
-    vim.api.nvim_set_hl(0, "DiagnosticSign", {bg = "none"})
+	--vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	--vim.api.nvim_set_hl(0, "LineNr", {bg = "none"})
+	--vim.api.nvim_set_hl(0, "SignColumn", {bg = "none"})
+	--vim.api.nvim_set_hl(0, "DiagnosticSign", {bg = "none"})
 end
 
 ColorMyPencils()
--- lua/renbrek/packer.lua
+++ lua/renbrek/packer.lua
@@ -1,42 +1,54 @@
-vim.cmd [[packadd packer.nvim]]
+vim.cmd([[packadd packer.nvim]])
 
-return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  
-  use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    -- or                            , branch = '0.1.x',
-    requires = { {'nvim-lua/plenary.nvim'} }
-  }
+return require("packer").startup(function(use)
+	-- Packer can manage itself
+	use("wbthomason/packer.nvim")
 
-  use "rebelot/kanagawa.nvim"
+	use({
+		"nvim-telescope/telescope.nvim",
+		tag = "0.1.2",
+		-- or                            , branch = '0.1.x',
+		requires = { { "nvim-lua/plenary.nvim" } },
+	})
 
-  use ('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
+	use("rebelot/kanagawa.nvim")
 
-  use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v2.x',
-  requires = {
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},             -- Required
-    {'williamboman/mason.nvim'},           -- Optional
-    {'williamboman/mason-lspconfig.nvim'}, -- Optional
+	use("nvim-treesitter/nvim-treesitter", { run = ":TSUpdate" })
 
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},         -- Required
-    {'hrsh7th/cmp-nvim-lsp'},     -- Required
-    {'hrsh7th/cmp-buffer'},       -- Optional
-    {'hrsh7th/cmp-path'},         -- Optional
-    {'saadparwaiz1/cmp_luasnip'}, -- Optional
-    {'hrsh7th/cmp-nvim-lua'},     -- Optional
+	use({
+		"VonHeikemen/lsp-zero.nvim",
+		branch = "v2.x",
+		requires = {
+			-- LSP Support
+			{ "neovim/nvim-lspconfig" }, -- Required
+			{ "williamboman/mason.nvim" }, -- Optional
+			{ "williamboman/mason-lspconfig.nvim" }, -- Optional
 
-    -- Snippets
-    {'L3MON4D3/LuaSnip'},             -- Required
-    {'rafamadriz/friendly-snippets'}, -- Optional
+			-- Autocompletion
+			{ "hrsh7th/nvim-cmp" }, -- Required
+			{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+			{ "hrsh7th/cmp-buffer" }, -- Optional
+			{ "hrsh7th/cmp-path" }, -- Optional
+			{ "saadparwaiz1/cmp_luasnip" }, -- Optional
+			{ "hrsh7th/cmp-nvim-lua" }, -- Optional
 
-    -- Formatters
-    { 'mhartington/formatter.nvim' }
-  }
-}
+			-- Snippets
+			{ "L3MON4D3/LuaSnip" }, -- Required
+			{ "rafamadriz/friendly-snippets" }, -- Optional
+
+			-- Formatters
+			{ "mhartington/formatter.nvim" },
+		},
+	})
+	use("petertriho/nvim-scrollbar")
+	use({
+		"windwp/nvim-autopairs",
+		config = function()
+			require("nvim-autopairs").setup({})
+		end,
+	})
+	use({ "windwp/nvim-ts-autotag" })
+	use({ "theprimeagen/harpoon" })
+	use({ "mbbill/undotree" })
+	use({ "tpope/vim-fugitive" })
 end)
