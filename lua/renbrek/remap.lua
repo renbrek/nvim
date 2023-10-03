058d80a0477af350ebb0b52b7d3f7edec2e4ac63
Ivan Bykov <renbrek@gmail.com>
2023-10-03 14:25:14 +0300

    feat: add remaps and backupcopy = "yes"

--- lua/renbrek/remap.lua
+++ lua/renbrek/remap.lua
@@ -1,2 +1,11 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+vim.keymap.set("x", "<leader>p", '"_dP')
+
+vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
+vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
+vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
+vim.keymap.set("n", "<leader>j", "<cmd>lprev<CR>zz")
+
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
--- lua/renbrek/set.lua
+++ lua/renbrek/set.lua
@@ -18,3 +18,4 @@ vim.opt.termguicolors = true
 vim.opt.scrolloff = 8
 
 vim.opt.swapfile = false
+vim.opt.backupcopy = "yes"
